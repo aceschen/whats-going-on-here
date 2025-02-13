2e5fa236565495a7010e958a64f7672ffd767375
Ace Chen <12aschen@berkeley.edu>
2025-02-12 17:00:13 -0800

    forgot the bg

--- src/data/background.ts
+++ src/data/background.ts
@@ -1,3 +1,10 @@
 import type { Option } from "../types/option";
+import { Slot } from "../types/slot";
 
-export const BACKGROUND_OPTIONS: Option[] = [];
+export const BACKGROUND_OPTIONS: Option[] = [
+  {
+	name: "old_offwhite",
+	asset: "old_offwhite.png",
+	slot: Slot.BACKGROUND,
+  },	
+];
